ceed1af27cf43adc0fd4a67c4b3e5e63898b5eb6
Michika <mhiratsuka00@mylangara.bc.ca>
2017-01-26 07:44:42 +0000

    modify assign4.js

--- assign4.js
+++ assign4.js
@@ -1,5 +1,4 @@
-const mealCost = process.argv[2];
-/*const mealCost = Number(process.argv[2]);*/
+const mealCost = Number(process.argv[2]);
 const tipPercent = 15;
 const tipAmount = (tipPercent / 100) * mealCost;
 const totalOwing = tipAmount + mealCost;
